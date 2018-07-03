cac607c300fc400214bcd7ef81fbab938de61034
amanthakur11 <amansingh8412@gmail.com>
2018-07-03 16:31:13 +0530

    Update trainembedding.py

--- trainembedding.py
+++ trainembedding.py
@@ -42,20 +42,22 @@ def create_train_data(pixel1,q3,kern,thresold):
         print(a)
         training_data.append([a,np.array(label)])
     if training_data:
-        print("\ntraining data is created.")
+        print("\ntraining data has been created.")
     shuffle(training_data)
     np.save("training_data.npy",training_data)
     return training_data
 
 
 def process_test_data(kern,thresold,strd):
-    img=cv2.resize(cv2.imread("/home/amanthakur/Downloads/train/cat.4.jpg",0),(img_size,img_size))
+    print("\nTesting Image!")
+    img_size1=150
+    img=cv2.resize(cv2.imread("/home/amanthakur/Downloads/test1/4.jpg",0),(img_size1,img_size1))
     plt.imshow(img,cmap="gray")
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
     pixel=np.array(img)
     print("\nprocessing testing data....")
-    blocks,index=dt.make_blocks(pixel,strd,kern,img_size)
+    blocks,index=dt.make_blocks(pixel,strd,kern,img_size1)
     q3,pixel1,e2=dt.diff_block_div_one(blocks)
     dt.make_dataset(pixel1,q3,kern,thresold,"testing_file.csv")
     test_dataset=pd.read_csv("testing_file.csv").values
@@ -71,9 +73,9 @@ def process_test_data(kern,thresold,strd):
         testing_data.append(a)
         y_test.append(img[-1])
     if testing_data:
-        print("\ntesting data is processed..")
-    np.save("test_data.npy",(testing_data,y_test))
-    return testing_data,y_test
+        print("\ntesting data has been processed..")
+    np.save("test_data.npy",(testing_data,y_test,index,img_size1))
+    return testing_data,y_test,index,img_size1
 
 def train_model(train_data,kern):
     
@@ -89,27 +91,27 @@ def train_model(train_data,kern):
 
     convnet = input_data(shape=[None, kern,kern, 1], name='input')
     #print(convnet)
-    convnet = conv_2d(convnet, 16, 2, activation='relu')
+    convnet = conv_2d(convnet, 32, 2, activation='tanh')
     #print(convnet)
     convnet = max_pool_2d(convnet, 2)
     #print(convnet)
     
-    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = conv_2d(convnet, 64, 2, activation='tanh')
     convnet = max_pool_2d(convnet, 2)
     
-    convnet = conv_2d(convnet, 16, 2, activation='relu')
+    convnet = conv_2d(convnet, 32, 2, activation='tanh')
     convnet = max_pool_2d(convnet, 2)
     
-    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = conv_2d(convnet, 64, 2, activation='tanh')
     convnet = max_pool_2d(convnet, 2)
     
-    convnet = conv_2d(convnet, 16, 2, activation='relu')
+    convnet = conv_2d(convnet, 32, 2, activation='tanh')
     convnet = max_pool_2d(convnet, 2)
     
-    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = conv_2d(convnet, 64, 2, activation='tanh')
     convnet = max_pool_2d(convnet, 2)
     #print(convnet)
-    convnet = fully_connected(convnet, 512, activation='relu')
+    convnet = fully_connected(convnet, 1024, activation='tanh')
     convnet = dropout(convnet, 0.8)
     
     convnet = fully_connected(convnet, 2, activation='softmax')
@@ -127,8 +129,10 @@ def train_model(train_data,kern):
         print("model loaded!")
     return model
 
-def test_model(model,test_data,kern):
+def test_model(model,test_data,kern,index,img_size1):
+    img=cv2.resize(cv2.imread("/home/amanthakur/Downloads/test1/4.jpg",0),(img_size1,img_size1))
     y_pred=[]
+    ind=0
     for data in test_data:
         img_data=data
         data=img_data.reshape(kern,kern,1)
@@ -137,7 +141,25 @@ def test_model(model,test_data,kern):
             str_label='NROI'
         else:
             str_label='ROI'
+        if str_label=='ROI':
+            blocks,index1=dt.make_blocks(img_data,1,2,kern)
+            ind2=0
+            #blocks=list(blocks)
+            for i in blocks:
+                s=sum(list(x).count(0) for x in i)
+                if s>=2:
+                    inx=index1[ind2]
+                    xx1,yy1=inx[0][0]
+                    xx2,yy2=inx[-1][-1]
+                    x1,y1=index[ind][xx1][yy1]
+                    x2,y2=index[ind][xx2][yy2]
+                    cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),1)
+                ind2+=1
         y_pred.append(str_label)
+        ind+=1
+    plt.imshow(img,cmap="gray")
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
     return y_pred
         
 def confusion_metrics(y_test,y_pred):
@@ -148,21 +170,22 @@ def accuracy(y_test,y_pred):
 
 
 thresold=10
+#kern=7
 pixel1,q3,kern,strd=dt.main(img_size,thresold)
 
 train_data=create_train_data(pixel1,q3,kern,thresold)
 #if you already have train data
 #train_data=np.load("training_data.npy")
 
-test_data,y_test=process_test_data(kern,thresold,strd)
-#test_data,y_test=np.load("test_data.npy")
-print("\ntrainging the model.")
+test_data,y_test,index,img_size1=process_test_data(kern,thresold,strd)
+#test_data,y_test,index,img_size1=np.load("test_data.npy")
+print("\ntraining the model.")
 model=train_model(train_data,kern)
-y_pred=test_model(model,test_data,kern)
+y_pred=test_model(model,test_data,kern,index,img_size1)
 
 matrix=confusion_metrics(y_test,y_pred)
 
-print("confusion matrix: ",matrix)
+print("confusion matrix: \n",matrix)
 
 print("accuracy of the model: ",accuracy(y_test,y_pred))
 
